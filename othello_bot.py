ece2e97543b8e5a5aa7758edbc417fd89a89206e
Cindy <74145775+ElemelonWind@users.noreply.github.com>
2022-02-04 18:13:23 -0500

    Update and rename Yang_C_U3_Lab3.py to othello_bot.py

--- othello_bot.py
+++ othello_bot.py
@@ -1,6 +1,3 @@
-# Name: Cindy Yang
-# Date: 12/15/2021
-
 import random
 import copy
 
@@ -41,49 +38,33 @@ class RandomBot:
 
    def find_moves(self, board, color):
     # finds all possible moves
-    moves = set()
+    moves_found = set()
     for i in range(self.x_max):
         for j in range(self.y_max):
-            legal = False 
-            for dir in self.directions:
-                curX = i+dir[0]
-                curY = j+dir[1]
-                if curX >= 0 and curY >= 0 and curX < self.x_max and curY < self.y_max and board[curX][curY] == self.opposite_color[color] and board[i][j] == ".":
-                  while curX >= 0 and curY >= 0 and curX < self.x_max and curY < self.y_max:
-                     # print(i, j, curX, curY)
-                     if board[curX][curY] == ".":
-                        break 
-                     if board[curX][curY] == self.opposite_color[color]:
-                        curX += dir[0]
-                        curY += dir[1]
-                     elif board[curX][curY] == color:
-                        moves.add((i, j))
-                        legal = True 
-                        break 
-                if legal: 
-                   continue
-    return moves
-
+            flipped_stones = self.find_flipped(board, i, j, color)
+            if len(flipped_stones) > 0:
+                moves_found.add((i, j))
+    return moves_found
 
    def find_flipped(self, board, x, y, color):
     # finds which chips would be flipped given a move and color
-      flipped = set()
-      for dir in self.directions:
-         curX = x+dir[0]
-         curY = y+dir[1]
-         toFlip = set()
-         while curX >= 0 and curY >= 0 and curX < self.x_max and curY < self.y_max:
-            if board[curX][curY] == self.opposite_color[color]:
-               toFlip.add((curX, curY))
-               curX += dir[0]
-               curY += dir[1]
-            elif board[curX][curY] == color:
-               flipped += toFlip 
-               break 
-            else:
-               break
-
-      return flipped
+    if board[x][y] != ".":
+        return []
+    flipped_stones = set()
+    for incr in self.directions:
+        temp_flip = set()
+        x_pos = x + incr[0]
+        y_pos = y + incr[1]
+        while 0 <= x_pos < self.x_max and 0 <= y_pos < self.y_max:
+            if board[x_pos][y_pos] == ".":
+                break
+            if board[x_pos][y_pos] == color:
+                flipped_stones = flipped_stones.union(temp_flip)
+                break
+            temp_flip.add((x_pos, y_pos))
+            x_pos += incr[0]
+            y_pos += incr[1]
+    return flipped_stones
 
 class Minimax_AI_bot:
 
@@ -163,48 +144,33 @@ class Minimax_AI_bot:
 
    def find_moves(self, board, color):
     # finds all possible moves
-      moves = set()
-      for i in range(self.x_max):
-         for j in range(self.y_max):
-               legal = False 
-               for dir in self.directions:
-                  curX = i+dir[0]
-                  curY = j+dir[1]
-                  if curX >= 0 and curY >= 0 and curX < self.x_max and curY < self.y_max and board[curX][curY] == self.opposite_color[color] and board[i][j] == ".":
-                     while curX >= 0 and curY >= 0 and curX < self.x_max and curY < self.y_max:
-                        # print(i, j, curX, curY)
-                        if board[curX][curY] == ".":
-                           break 
-                        if board[curX][curY] == self.opposite_color[color]:
-                           curX += dir[0]
-                           curY += dir[1]
-                        elif board[curX][curY] == color:
-                           moves.add((i, j))
-                           legal = True 
-                           break 
-                  if legal: 
-                     continue
-      return moves
+    moves_found = set()
+    for i in range(self.x_max):
+        for j in range(self.y_max):
+            flipped_stones = self.find_flipped(board, i, j, color)
+            if len(flipped_stones) > 0:
+                moves_found.add((i, j))
+    return moves_found
 
    def find_flipped(self, board, x, y, color):
     # finds which chips would be flipped given a move and color
-      flipped = set()
-      for dir in self.directions:
-         curX = x+dir[0]
-         curY = y+dir[1]
-         toFlip = set()
-         while curX >= 0 and curY >= 0 and curX < self.x_max and curY < self.y_max:
-            if board[curX][curY] == self.opposite_color[color]:
-               toFlip.add((curX, curY))
-               curX += dir[0]
-               curY += dir[1]
-            elif board[curX][curY] == color:
-               flipped = flipped.union(toFlip)
-               break 
-            else:
-               break
-
-      return flipped
+    if board[x][y] != ".":
+        return []
+    flipped_stones = set()
+    for incr in self.directions:
+        temp_flip = set()
+        x_pos = x + incr[0]
+        y_pos = y + incr[1]
+        while 0 <= x_pos < self.x_max and 0 <= y_pos < self.y_max:
+            if board[x_pos][y_pos] == ".":
+                break
+            if board[x_pos][y_pos] == color:
+                flipped_stones = flipped_stones.union(temp_flip)
+                break
+            temp_flip.add((x_pos, y_pos))
+            x_pos += incr[0]
+            y_pos += incr[1]
+    return flipped_stones
 
 class Alpha_beta_AI_bot:
 
@@ -288,48 +254,33 @@ class Alpha_beta_AI_bot:
 
    def find_moves(self, board, color):
     # finds all possible moves
-      moves = set()
-      for i in range(self.x_max):
-         for j in range(self.y_max):
-               legal = False 
-               for dir in self.directions:
-                  curX = i+dir[0]
-                  curY = j+dir[1]
-                  if curX >= 0 and curY >= 0 and curX < self.x_max and curY < self.y_max and board[curX][curY] == self.opposite_color[color] and board[i][j] == ".":
-                     while curX >= 0 and curY >= 0 and curX < self.x_max and curY < self.y_max:
-                        # print(i, j, curX, curY)
-                        if board[curX][curY] == ".":
-                           break 
-                        if board[curX][curY] == self.opposite_color[color]:
-                           curX += dir[0]
-                           curY += dir[1]
-                        elif board[curX][curY] == color:
-                           moves.add((i, j))
-                           legal = True 
-                           break 
-                  if legal: 
-                     continue
-      return moves
+    moves_found = set()
+    for i in range(self.x_max):
+        for j in range(self.y_max):
+            flipped_stones = self.find_flipped(board, i, j, color)
+            if len(flipped_stones) > 0:
+                moves_found.add((i, j))
+    return moves_found
 
    def find_flipped(self, board, x, y, color):
     # finds which chips would be flipped given a move and color
-      flipped = set()
-      for dir in self.directions:
-         curX = x+dir[0]
-         curY = y+dir[1]
-         toFlip = set()
-         while curX >= 0 and curY >= 0 and curX < self.x_max and curY < self.y_max:
-            if board[curX][curY] == self.opposite_color[color]:
-               toFlip.add((curX, curY))
-               curX += dir[0]
-               curY += dir[1]
-            elif board[curX][curY] == color:
-               flipped = flipped.union(toFlip)
-               break 
-            else:
-               break
-
-      return flipped
+    if board[x][y] != ".":
+        return []
+    flipped_stones = set()
+    for incr in self.directions:
+        temp_flip = set()
+        x_pos = x + incr[0]
+        y_pos = y + incr[1]
+        while 0 <= x_pos < self.x_max and 0 <= y_pos < self.y_max:
+            if board[x_pos][y_pos] == ".":
+                break
+            if board[x_pos][y_pos] == color:
+                flipped_stones = flipped_stones.union(temp_flip)
+                break
+            temp_flip.add((x_pos, y_pos))
+            x_pos += incr[0]
+            y_pos += incr[1]
+    return flipped_stones
 
 class Best_AI_bot:
 
@@ -381,33 +332,6 @@ class Best_AI_bot:
                v = (curX, curY), cur[1]
          if v[1] < alpha: return v 
          beta = min(beta, v[1])
-      return v
-
-   def minimax(self, board, color, search_depth):
-    # returns best "value"
-      sol = self.max_value(board, color, search_depth, -1, -1, -1, -1)
-      return sol
-
-   def max_value(self, board, color, search_depth, maxX, maxY, minX, minY):
-      if search_depth == 0: return (maxX, maxY), self.evaluate(board, color)
-      moves = self.find_moves(board, color)
-      if len(moves) == 0: return (-1, -1), self.evaluate(board, color) 
-      v = ((maxX, maxY), float("-inf"))
-      for curX, curY in moves:
-         cur = self.min_value(self.make_move(copy.deepcopy(board), color, curX, curY, self.find_flipped(board, curX, curY, color)), color, search_depth-1, curX, curY, minX, minY)
-         if cur[1] > v[1]:
-               v = (curX, curY), cur[1]
-      return v
-
-   def min_value(self, board, color, search_depth, maxX, maxY, minX, minY):
-      if search_depth == 0: return (minX, minY), self.evaluate(board, color)
-      moves = self.find_moves(board, self.opposite_color[color])
-      if len(moves) == 0: return (-1, -1), self.evaluate(board, color) 
-      v = ((minX, minY), float("inf"))
-      for curX, curY in moves:
-         cur = self.max_value(self.make_move(copy.deepcopy(board), self.opposite_color[color], curX, curY, self.find_flipped(board, curX, curY, self.opposite_color[color])), color, search_depth-1, maxX, maxY, curX, curY)
-         if cur[1] < v[1]:
-               v = (curX, curY), cur[1]
       return v
 
    def stones_left(self, board):
